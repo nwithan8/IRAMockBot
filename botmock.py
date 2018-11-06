358d1040d1dc8b8443fe9ee6d490be3da57b7990
Nate Harris <nwithan8@users.noreply.github.com>
2018-11-06 17:16:20 -0500

    Fix incorrect comments

--- botmock.py
+++ botmock.py
@@ -9,9 +9,9 @@ consumer_secret = ' '
 access_token = ' '
 access_secret = ' '
 
-#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-#auth.set_access_token(access_token, access_secret)
-#api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
 
 tweet = ""
 
@@ -26,7 +26,6 @@ while True:
 #Produce 1 tweet-length (280 characters) sentence
     for i in range(1):
         tweet = text_model.make_short_sentence(280)
-        print(tweet)
-        #api.update_status(tweet) #Post tweet
+        api.update_status(tweet) #Post tweet
         time.sleep(10) #Tweet every 30 minutes
 
